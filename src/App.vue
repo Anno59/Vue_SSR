bf3d277207dbb9936ce5ed7e9c47257238136725
yubolin <anno59@163.com>
2020-02-09 20:39:51 +0800

    add event component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Event></Event>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Event from './components/Event'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Event
   }
 }
 </script>
---
+++ src/components/Event.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p @click="msg">test</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Event",
+    data(){
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      msg() {
+        this.count += 1;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
